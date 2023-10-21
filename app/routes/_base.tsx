dceddafbd6cacc7c7e36fb2a46b4dfd4659f31df
Christopher Tian <mechristophertian@gmail.com>
2023-10-21 16:49:20 +0200

    Redirect from _base layout to _auth if user is not logged in

--- app/routes/_base.tsx
+++ app/routes/_base.tsx
@@ -1,13 +1,28 @@
-import type { MetaFunction } from '@remix-run/node'
+import {
+  redirect,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from '@remix-run/node'
 import { Outlet } from '@remix-run/react'
 import { MobileNavigation } from '~/components/mobile-navigation'
 import { TopBar } from '~/components/top-bar'
+import { getUserId } from '~/utils/user-session.server'
 
 export const meta: MetaFunction = () => {
   return [
     { title: 'Twitter Clone' },
     { name: 'description', content: 'Twitter clone made with Remix' },
   ]
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const userId = await getUserId(request)
+
+  if (!userId) {
+    return redirect('/auth')
+  }
+
+  return null
 }
 
 export default function BaseLayout() {
